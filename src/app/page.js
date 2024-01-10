7619f9ebd89637bf69fb2b86f609361687800d24
Ravan <iamdinarbey@gmail.com>
2024-01-10 22:31:39 +0300

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,9 @@ import Image from 'next/image'
 export default function Home() {
     return (
         <div className="">
+            <head>
+                <title>Ravan Babayev</title>
+            </head>
             <div className="flex flex-col text-center justify-center space-y-12">
                 <div className="w-full text-center flex justify-center">
                     <Image src="/avatar.webp" alt="me" height="172" width="172" className="select-none"></Image>
@@ -12,7 +15,8 @@ export default function Home() {
                     <h1 className="title">FULLSTACK DEVELOPER</h1>
                 </div>
                 <div className="pt-16">
-                    <a href="http://api.whatsapp.com/send?phone=905397312328" target="_blank" rel="noreferrer" className="px-8 py-3 rounded-full inline-flex justify-center items-center space-x-2 transition-all group select-none ring ring-0 hover:ring-4 transition-all duration-200 lg:px-16 lg:py-6 lg:hover:ring-8 bg-blue-500 ring-blue-300">
+                    <a href="http://api.whatsapp.com/send?phone=905397312328" target="_blank" rel="noreferrer"
+                       className="px-8 py-3 rounded-full inline-flex justify-center items-center space-x-2 transition-all group select-none ring ring-0 hover:ring-4 transition-all duration-200 lg:px-16 lg:py-6 lg:hover:ring-8 bg-blue-500 ring-blue-300">
                     <span className="text-sm leading-6 font-semibold text-white lg:text-2xl lg:leading-8 text-white">
                         Say hellooo
                     </span>
@@ -40,7 +44,8 @@ export default function Home() {
             </div>
 
             <div className="absolute top-8 right-16">
-                <a href="https://github.com/ravanbabayev" className="inline-flex items-center space-x-1" target="_blank">
+                <a href="https://github.com/ravanbabayev" className="inline-flex items-center space-x-1"
+                   target="_blank">
                     <span className="robotoMono text-white uppercase">Github</span>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="h-5 w-5 text-white">
                         <path fill="none" d="M0 0h256v256H0z"/>
